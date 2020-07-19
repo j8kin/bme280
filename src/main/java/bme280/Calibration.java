a689e114c7fd2ee0b4bcb5820cd46208f0f284ba
j8kin <emk161278@gmail.com>
2020-07-19 21:54:05 +0300

    Update calibration parameters. Add information about fine resolution for compensation formula

--- src/main/java/bme280/Calibration.java
+++ src/main/java/bme280/Calibration.java
@@ -1,47 +1,45 @@
 package bme280;
 
 public enum Calibration {
-    /* Temperature Calibration Parameters */
-    /** unsigned 16 bit integer */
+    /** Temperature Calibration Parameter 1 (unsigned 16 bit integer) */
     digT1,
-    /** signed 16 bit integer */
+    /** Temperature Calibration Parameter 2 (signed 16 bit integer) */
     digT2,
-    /** signed 16 bit integer */
+    /** Temperature Calibration Parameter 3 (signed 16 bit integer) */
     digT3,
-    /* Pressure Calibration Parameters */
-    /** unsigned 16 bit integer */
+    
+    /** Pressure Calibration Parameter 1 (unsigned 16 bit integer) */
     digP1,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 2 (signed 16 bit integer) */
     digP2,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 3 (signed 16 bit integer) */
     digP3,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 4 (signed 16 bit integer) */
     digP4,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 5 (signed 16 bit integer) */
     digP5,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 6 (signed 16 bit integer) */
     digP6,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 7 (signed 16 bit integer) */
     digP7,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 8 (signed 16 bit integer) */
     digP8,
-    /** signed 16 bit integer */
+    /** Pressure Calibration Parameter 9 (signed 16 bit integer) */
     digP9,
-    /* Humidity Calibration Parameters */
-    /** unsigned 8 bit */
+
+    /** Humidity Calibration Parameter 1 (unsigned 8 bit) */
     digH1,
-    /** signed 16 bit integer */
+    /** Humidity Calibration Parameter 2 (signed 16 bit integer) */
     digH2,
-    /** unsigned 8 bit */
+    /** Humidity Calibration Parameter 3 (unsigned 8 bit) */
     digH3,
-    /** signed 16 bit integer */
+    /** Humidity Calibration Parameter 4 (signed 16 bit integer) */
     digH4,
-    /** signed 16 bit integer */
+    /** Humidity Calibration Parameter 5 (signed 16 bit integer) */
     digH5,
-    /** signed 8 bit integer */
+    /** Humidity Calibration Parameter 6 (signed 8 bit integer) */
     digH6,
 
-    /* Unknown Parameter */
-    /** signed 32 bit integer */
+    /** a fine resolution temperature value over to the pressure and humidity compensation formula (signed 32 bit integer) */
     tFine;
 }
